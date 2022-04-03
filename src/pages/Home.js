d06f618ba93bda45ba993ed803d43147332b8f84
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-03 18:31:45 -0500

    added header skeleton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Header from '../components/UtilComp/Header/Header'
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Header />
+    </div>
   )
 }
 
