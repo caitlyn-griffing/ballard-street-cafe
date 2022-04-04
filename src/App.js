ef126566d9c931316c02f2a398d30ca3cbe97e62
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-04 10:14:58 -0500

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Home from './pages/Home';
-
+import { Routes, Route } from "react-router-dom";
 import './fonts/Marlboro.ttf';
-
+import { Home, Breakfast, Dinner, Lunch, Pictures } from './pages';
 
 function App() {
   //Neil
   return (
     <div className="App">
-        <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/breakfast' element={<Breakfast />}/>
+        <Route path='/lunch' element={<Lunch />}/>
+        <Route path='/dinner' element={<Dinner />}/>
+        <Route path='/pictures' element={<Pictures />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Breakfast from './Breakfast';
+import Home from './Home';
+import Lunch from './Lunch';
+import Dinner from './Dinner';
+import Pictures from './Pictures';
+
+export {
+    Home,
+    Lunch,
+    Dinner,
+    Pictures,
+    Breakfast
+}
