a8513e47f5666b1cc351738384abce1fa7856354
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-03 19:10:46 -0500

    added navbar

--- src/components/UtilComp/Navbar/Navbar.js
+++ src/components/UtilComp/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import navbarItems from './NavbarItems'
-
+import './Navbar.css'
 function Navbar() {
   return (
-    <div>
+    <div id='navbar'>
         <ul>
         {
-            navbarItems.map((item) => {
+            navbarItems.map(({ cName, src, name }) => {
                 return(
-                    <li><a href={item.src}>{item.name}</a></li>
+                    <li className={cName}><a href={src}>{name}</a></li>
                 )
             })
         }
