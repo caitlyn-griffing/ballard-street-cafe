b21194d17a666ee29de9ab2b2377b7ae65777d08
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-16 00:40:06 -0500

    client commit to complete api fetch

--- src/pages/Pictures.js
+++ src/pages/Pictures.js
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Pictures() {
-
+  const [data, setData] = useState()
+  //Mount handling is essential for this to work
   useEffect( () => {
-    fetch('/api')
-    .then(res => {
-      console.log(res.headers)
+    let mounted = true;
+    const response = fetch('/api');
+    const dataInflow = response.then(res => {
+      console.log(res)
       return res.json()
-    })
-    .then(data => {
+    });
+    dataInflow.then(data => {
       console.log(data)
+      if(mounted)
+        setData(data.reviews)
     })
+    .catch(err => console.log(err))
+    return () => mounted = false;
   }, [])
 
-
+  //Add ternary operator to handle lazy loading. If not used then the page will load before the data is fetched, resulting in a response that says "enable javsciprt to run this app"
   return (
-    <div>dd</div>
+    <div>{data ? 
+      data.map((review, index) => {
+        return (
+          <div key={index}>
+            <h1>{review.author_name}</h1>
+            <p>{review.text}</p>
+          </div>
+        )
+      }) : 
+      <>Loading...</>}
+    </div>
   )
 }
 
